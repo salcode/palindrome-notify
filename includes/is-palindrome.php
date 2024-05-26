736f09ae1c8e8f1eecbf96a070e65e8e11b99f39
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-05-26 06:06:24 -0400

    Modify is_palindrome() to ignore case
    Modify is_palindrome() to ignore case, i.e. to be case-insensitive

--- includes/is-palindrome.php
+++ includes/is-palindrome.php
@@ -9,5 +9,6 @@ namespace salcode\PalindromeNotify;
  * @return bool True if the string is a palindrome.
  */
 function is_palindrome( $str ) {
-	return $str === strrev( $str );
+	$lc_str = strtolower( $str );
+	return $lc_str === strrev( $lc_str );
 }
