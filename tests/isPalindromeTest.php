d0a88aec10a7f65155e206a855478e1f85070318
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-05-26 14:25:19 -0400

    Refactor testIsPalindrome() to use dataProvider
    Refactor isPalindromeTest::testIsPalindrome() to use a dataProvider

--- tests/isPalindromeTest.php
+++ tests/isPalindromeTest.php
@@ -1,16 +1,29 @@
 <?php
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 use function salcode\PalindromeNotify\is_palindrome;
 
 class isPalindromeTest extends TestCase {
-    public function testIsPalindrome() {
-		$str = 'racecar';
-		$expected = true;
+
+	/**
+	 * Test is_palindrome()
+	 *
+	 * @param string $str The string to test if it is a palindrome.
+	 * @param bool   $expected The expected result (true or false).
+	 */
+	#[DataProvider('isPalindromeProvider')]
+    public function testIsPalindrome( $str, $expected ) {
         $this->assertEquals(
             $expected,
 			is_palindrome( $str ),
         );
     }
+
+	public static function isPalindromeProvider() {
+		return [
+			[ 'racecar', true ],
+		];
+	}
 }
